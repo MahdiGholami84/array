4096945f1883034eca1dc077f340177cdd9c7a3f
Mahdi <mahdinomad75@gmail.com>
2022-11-29 12:04:22 +0330

    پیدا کردن پایتخت یک کشور وارد شده در ورودی

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <?php
-$c = array('Iran', 'Iraq', 'Turkey', 'Russia' , 'ugugb' , 'jhghjyi' , 'hgfhjtfut');
+$c = array('Iran', 'Iraq', 'Turkey', 'Russia');
 $p = array('Tehran', 'Baghdad', 'Ankara', 'Moscow');
-/*for ($a = 0; $a < count($c); $a++) {
-    $c[$a] = $a;
-}
-*/
-for ($a = 0; $a < count($c); $a++) {
-    echo $c[$a] . "<br>";
-}
+$find = 0;
+if (isset($_POST['submit'])) {
+    $country = $_POST['country'];
+    for ($i = 0; $i < count($c); $i++) {
+        if ($c[$i] == $country) {
+            $find = 1;
+            break;
+        }//if
+    }//for
+
+    if ($find == 1) {
+        echo $p[$i];
+    } else {
+        echo "پیدا نشد";
+    }
+}//if
 ?>
+<form action="" method="post">
+    <label>:نام کشور</label> <input type="text" name="country"/>
+    <input type="submit" name="submit" value="پایتخت"/>
+</form>
