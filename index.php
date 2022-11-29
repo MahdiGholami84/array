f0e2c321a4db5398bfabb6d0237282a18bec1850
Mahdi <mahdinomad75@gmail.com>
2022-11-29 11:43:12 +0330

    حلقه با آرایه

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$c = array('Iran', 'Iraq', 'Turkey', 'Russia' , 'ugugb' , 'jhghjyi' , 'hgfhjtfut');
+$p = array('Tehran', 'Baghdad', 'Ankara', 'Moscow');
+/*for ($a = 0; $a < count($c); $a++) {
+    $c[$a] = $a;
+}
+*/
+for ($a = 0; $a < count($c); $a++) {
+    echo $c[$a] . "<br>";
+}
+?>
